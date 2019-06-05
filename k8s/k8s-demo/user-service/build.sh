b6abd0c6e72905f6fbe356da8177ceff7efebc60
OA\hz19033950 <hytmars1989@126.com>
2019-06-05 09:56:56 +0800

    yaml

--- k8s/k8s-demo/user-service/build.sh
+++ k8s/k8s-demo/user-service/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 mvn clean package
-
+TIME=`date "+%Y%m%d%H%M"`;
+GIT_REVISION=`git log -1 --pretty=format:%h`;
 docker login 10.160.2.15 -u hz1203298 -p 340251
-docker build -t 10.160.2.83:6110/trip/user-service:latest .
+docker build -t 10.160.2.83:6110/trip/user-service:${TIME}_${GIT_REVISION} .
 docker logout
-docker login 10.160.2.83 -u admin -p Harbor12345
-docker push 10.160.2.83:6110/trip/user-service:latest
+docker login 10.160.2.83:6110 -u admin -p Harbor12345
+docker push 10.160.2.83:6110/trip/user-service:${TIME}_${GIT_REVISION}
 docker logout
