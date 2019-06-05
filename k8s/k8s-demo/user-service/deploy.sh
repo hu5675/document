30acb0a2ae6cea466ea9e4bd415b7d75dfa74731
OA\hz19033950 <hytmars1989@126.com>
2019-06-05 10:26:29 +0800

    deploy.sh

--- k8s/k8s-demo/user-service/deploy.sh
+++ k8s/k8s-demo/user-service/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 IMAGE=`cat IMAGE_NAME`;
 echo "update image to :${IMAGE}";
-kubect set image deployment/user-service-deployment user-service=${IMAGE}
+kubectl set image deployment/user-service-deployment user-service=${IMAGE}
