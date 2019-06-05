41acd976f0f10d67efdcd7007fbb34eda40057d9
OA\hz19033950 <hytmars1989@126.com>
2019-06-05 10:47:24 +0800

    deploy.sh + user-service.yaml

--- k8s/k8s-demo/user-service/deploy.sh
+++ k8s/k8s-demo/user-service/deploy.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 IMAGE=`cat IMAGE_NAME`;
-echo "update image to :${IMAGE}";
-kubectl set image deployment/user-service-deployment user-service=${IMAGE}
+COUNT=`kubectl get deploy --show-labels | grep app=user-service | wc -l`;
+if [[ ${COUNT} -gt 0 ]]; then
+    echo "update image to :${IMAGE}";
+    kubectl set image deployment/user-service-deployment user-service=${IMAGE}
+else
+    sed -i 's/$image/${IMAGE}/g' user-service.yaml
+    kubectl create -f user-service.yaml
+fi
