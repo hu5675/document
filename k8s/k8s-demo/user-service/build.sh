b84d76a2264a1645b5949dbbebdc95c7b2c0eb3f
OA\hz19033950 <hytmars1989@126.com>
2019-06-05 10:18:03 +0800

    deploy.sh

--- k8s/k8s-demo/user-service/build.sh
+++ k8s/k8s-demo/user-service/build.sh
@@ -2,9 +2,12 @@
 maven clean package -Dautoconfig.skip=true -Dmaven.test.skip=true;
 TIME=`date "+%Y%m%d%H%M"`;
 GIT_REVISION=`git log -1 --pretty=format:%h`;
+IMAGE_NAME=10.160.2.83:6110/trip/user-service:${TIME}_${GIT_REVISION};
 docker login 10.160.2.15 -u hz1203298 -p 340251
-docker build -t 10.160.2.83:6110/trip/user-service:${TIME}_${GIT_REVISION} .
+docker build -t ${IMAGE_NAME} .
 docker logout
 docker login 10.160.2.83:6110 -u admin -p Harbor12345
-docker push 10.160.2.83:6110/trip/user-service:${TIME}_${GIT_REVISION}
+docker push ${IMAGE_NAME}
 docker logout
+
+echo "${IMAGE_NAME}" > IMAGE_NAME
