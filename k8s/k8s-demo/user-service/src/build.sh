9c3eea0bd71e5f58875e5bafbbb4e039b5b02cfc
OA\hz19033950 <hytmars1989@126.com>
2019-06-05 09:06:14 +0800

    build.sh modify

--- k8s/k8s-demo/user-service/src/build.sh
+++ k8s/k8s-demo/user-service/src/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 mvn clean package
 
+docker login 10.160.2.15 -u hz1203298 -p 340251
 docker build -t 10.160.2.83:6110/trip/user-service:latest .
+docker logout
 docker login 10.160.2.83 -u admin -p Harbor12345
 docker push 10.160.2.83:6110/trip/user-service:latest
+docker logout
