fc3c29e44b2b799476e9d7fa168b75b41907df18
OA\hz19033950 <hytmars1989@126.com>
2019-06-05 19:54:02 +0800

    edge

--- k8s/k8s-demo/user-edge-service/src/main/java/com/mars/k8s/user/edge/service/UserService.java
+++ k8s/k8s-demo/user-edge-service/src/main/java/com/mars/k8s/user/edge/service/UserService.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service:8181")
 public interface UserService {
 
     @GetMapping("/user-service/user/list")
