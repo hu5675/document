37834e517076c98546bde27367f216ab7e36497d
OA\hz19033950 <hytmars1989@126.com>
2019-07-26 10:57:11 +0800

    本地user-service  调用 k8s message-service

--- k8s/k8s-demo/user-service/src/main/java/com/mars/k8s/user/service/MessageService.java
+++ k8s/k8s-demo/user-service/src/main/java/com/mars/k8s/user/service/MessageService.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @FeignClient(name = "message-service",url = "${message.service}")
+//@FeignClient(name = "message-service")
 public interface MessageService {
 
     @GetMapping("/message-service/message/list")
