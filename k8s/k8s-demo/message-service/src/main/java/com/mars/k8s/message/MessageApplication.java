147740386d57393da7860834cdf599e0be7da477
OA\hz19033950 <hytmars1989@126.com>
2019-09-24 11:04:30 +0800

    bbb

--- k8s/k8s-demo/message-service/src/main/java/com/mars/k8s/message/MessageApplication.java
+++ k8s/k8s-demo/message-service/src/main/java/com/mars/k8s/message/MessageApplication.java
@@ -1,18 +1,20 @@
 package com.mars.k8s.message;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
+//@EnableDiscoveryClient
+//@EnableFeignClients
 public class MessageApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MessageApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApplication.class, args);
+    }
 
 }
 
